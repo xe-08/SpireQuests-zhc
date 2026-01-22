4ac1abaebb10099991eeffd47b12c13101e01383
modargo <62083413+modargo@users.noreply.github.com>
2026-01-21 23:23:56 -0500

    Addictive Personality quest: avoid crashes if the logic for modifying JAX's description doesn't work as expected (#224)

--- src/main/java/spireQuests/quests/dayvig/patches/JAXCardDisplayPatch.java
+++ src/main/java/spireQuests/quests/dayvig/patches/JAXCardDisplayPatch.java
@@ -6,23 +6,27 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.colorless.JAX;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import spireQuests.Anniv8Mod;
 import spireQuests.quests.dayvig.relics.MutagenBlood;
 
 import java.util.ArrayList;
 
 public class JAXCardDisplayPatch {
     @SpirePatch(clz = AbstractCard.class, method = "renderDescription", paramtypez = SpriteBatch.class)
-
     public static class JAXModifyDescription {
-
         public static ArrayList<AbstractCard> cardsModified = new ArrayList<>();
 
         public static void Prefix(AbstractCard __instance, SpriteBatch sb) {
             if (__instance.cardID.equals(JAX.ID) && CardCrawlGame.isInARun() && AbstractDungeon.player.hasRelic(MutagenBlood.ID) && !cardsModified.contains(__instance)) {
                 String unmodifiedDescription = __instance.rawDescription;
                 int firstThreeIndex = __instance.rawDescription.indexOf('3');
-                __instance.rawDescription = unmodifiedDescription.substring(0, firstThreeIndex) + '1' + unmodifiedDescription.substring(firstThreeIndex+1);
-                __instance.initializeDescription();
+                if (firstThreeIndex != -1) {
+                    __instance.rawDescription = unmodifiedDescription.substring(0, firstThreeIndex) + '1' + unmodifiedDescription.substring(firstThreeIndex+1);
+                    __instance.initializeDescription();
+                }
+                else {
+                    Anniv8Mod.logger.warn("Could not find number to modify in Jax's description. Raw description: " + __instance.rawDescription);
+                }
                 cardsModified.add(__instance);
             }
         }
